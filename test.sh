18bffc7044e8ccd98b098c519a43dce3c46e5e02
Maximilian Hammerl <maximilian.hammerl@smart-cybersecurity.de>
2025-03-12 15:36:03 +0100

    Workflow

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/test' \
+--header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+--header 'Content-Type: application/json' \
